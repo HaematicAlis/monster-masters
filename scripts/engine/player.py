c604be66588e894c80a160a1f671ed0712d7f377
Blaze Wiseman <kawaiimicrowaves930@gmail.com>
2026-02-22 14:18:57 -0500

    Added recycle, special, fight zones to player

--- scripts/engine/player.py
+++ scripts/engine/player.py
@@ -6,6 +6,9 @@ class Player:
         self.pid = pid
         self.deck_zone = Zone(f"deck_{pid}", pid)
         self.hand_zone = Zone(f"hand_{pid}", pid)
+        self.recycle_zone = Zone(f"recycle_{pid}", pid)
+        self.special_zone = Zone(f"special_{pid}", pid)
+        self.fight_zone = Zone(f"fight_{pid}", pid)
 
     def __str__(self):
         return f"PID{self.pid}: {self.name}"
